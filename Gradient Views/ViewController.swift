cd8f8911133d281b8fe675d0b95dc7db406eb05c
zackaryoconnor <zackary25@gmail.com>
2018-06-18 20:28:13 -0700

    Initial Commit

---
+++ Gradient Views/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Gradient Views
+//
+//  Created by Zackary O'Connor on 6/18/18.
+//  Copyright © 2018 Zackary O'Connor. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
